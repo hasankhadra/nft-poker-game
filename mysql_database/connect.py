0a52ed30e58aa2d6a9aff6d674592c5ca8d8a9a5
hasankhadra <hasankhadra2013@gmail.com>
2022-03-08 20:56:42 +0300

    Implementing database functionality

---
+++ mysql_database/connect.py
@@ -0,0 +1,61 @@
+import MySQLdb
+import configparser
+
+class Connect:
+    
+    def __init__(self, file, db=None):
+        config = configparser.ConfigParser()
+        
+        config.read(file)
+        if db is None:
+            self.conn = MySQLdb.connect(
+                host=config['mysql']['host'],
+                port=int(config['mysql']['port']),
+                user=config['mysql']['user'],
+                passwd=config['mysql']['password'])
+        else:
+            self.conn = MySQLdb.connect(
+                host=config['mysql']['host'],
+                port=int(config['mysql']['port']),
+                db=db,
+                user=config['mysql']['user'],
+                passwd=config['mysql']['password'])
+        
+    def init(self):
+        crsr = self.conn.cursor()
+        return self.conn, crsr
+
+
+class Init:
+    
+    def __init__(self, file):
+        self.config_file = file
+        self.connect = Connect(self.config_file)
+    
+    def init(self, db_name):
+        if not self.is_db_exist(db_name):
+            self.create_db(db_name)
+            
+        connect = Connect(self.config_file, db_name)
+        return connect.init()
+        
+    def is_db_exist(self, db_name):
+        conn, crsr = self.connect.init()
+        crsr.execute("SHOW DATABASES;")
+        databases = crsr.fetchall()
+        databases = [item[0] for item in databases]
+        return db_name in databases
+        
+    def create_db(self, db_name):
+        conn, crsr = self.connect.init()
+        crsr.execute(f"CREATE DATABASE {db_name}")
+        conn.commit()
+        conn.close()
+        
+
+
+if __name__ == "__main__":
+    x = Init("db.ini")
+    conn, crsr = x.init("nft_poker_game")
+    
+    
---
+++ mysql_database/players.py
@@ -0,0 +1,106 @@
+from connect import Init
+
+class Players:
+    
+    def __init__(self, file):
+        self.db = 'nft_poker_game'
+        self.config_file = file
+        self.connect = Init(self.config_file)
+        if not self.is_players_exist():
+            self.create_table()
+        
+    def init(self):
+        return self.connect.init(self.db)
+        
+    def is_players_exist(self):
+        conn, crsr = self.init()
+        crsr.execute("show tables;")
+        tables = crsr.fetchall()
+        tables = [item[0] for item in tables]
+        return 'players' in tables  
+    
+    def delete_table(self):
+        conn, crsr = self.init()
+
+        crsr.execute("DROP TABLE players")
+        
+        conn.commit()
+        conn.close()
+    
+    def clear_table(self):
+        conn, crsr = self.init()
+        
+        crsr.execute("DELETE FROM players")
+        
+        conn.commit()
+        conn.close()
+    
+    def create_table(self):
+        conn, crsr = self.init()
+        
+        crsr.execute("""
+                     CREATE TABLE IF NOT EXISTS players (
+                         id INT AUTO_INCREMENT PRIMARY KEY, 
+                         public_address varchar(255),
+                         full_name VARCHAR(255), 
+                         round_num INT,
+                         is_rail BOOLEAN);
+                     """)
+        conn.commit()
+        conn.close()
+    
+    def add_player(self, player_info: list):
+        """
+        :param player_info: list containing [public_address, full_name]
+        """
+        
+        # add round_num and is_rail
+        player_info.append(0)
+        player_info.append(False)
+        
+        conn, crsr = self.init()
+        crsr.execute("""INSERT INTO players (public_address, full_name, round_num, is_rail) 
+                     VALUES (%s, %s, %s, %s)""", player_info)
+        
+        conn.commit()
+        conn.close()        
+    
+    def retrieve_players(self, limit: int):
+        conn, crsr = self.init()
+        
+        crsr.execute("SELECT * FROM players limit %s", [limit])
+        return crsr.fetchall()
+    
+    def create_index(self):
+        conn, crsr = self.init()
+        # TODO
+    
+    def update(self, to_update_info: dict):
+        conn, crsr = self.init()
+        
+        public_address = to_update_info["public_address"]
+        del to_update_info["public_address"]
+        
+        keys = list(to_update_info.keys())
+        values = list(to_update_info.values())
+        
+        update_fields_expression = ""
+        for item in keys:
+            update_fields_expression += item + " = " + "%s, "
+        update_fields_expression = update_fields_expression[:-2]
+        
+        values.append(public_address)
+        crsr.execute(f"UPDATE players SET {update_fields_expression} WHERE public_address = %s", values)
+        
+        conn.commit()
+        conn.close()
+
+if __name__ == "__main__":
+    players_instance = Players('db.ini')
+    players_instance.delete_table()
+    players_instance.create_table()
+    
+    players_instance.add_player(["address_1", "first last"])
+    players_instance.update({"round_num": 3, "public_address": "address_1", "full_name": "second last", "is_rail": True})
+    
+    
