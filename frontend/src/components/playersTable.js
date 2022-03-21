acdaedc176a3010e0fdd7b515fce3184613a0f63
hasankhadra <hasankhadra2013@gmail.com>
2022-03-21 22:44:24 +0300

    Fixing scoreboard

--- frontend/src/components/playersTable.js
+++ frontend/src/components/playersTable.js
@@ -8,13 +8,15 @@ function PlayersTable(props){
             <table>
                 <tr>
                     <th>Username</th>
-                    <th>Bounty</th>
+                    <th># of Round</th>
+                    <th>Total Bounty (USDT)</th>
                 </tr>
                 {
                     props.players.map(element => {
                         return (
                             <tr key={element.id}>
                             <td>{element.username}</td>
+                            <td>{element.Round_number}</td>
                             <td>{element.bounty}</td>
                             </tr>
                         )
