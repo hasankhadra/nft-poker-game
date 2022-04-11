83e343e6afe2daef05b7f082aed61b4ab7c33275
hasankhadra <hasankhadra2013@gmail.com>
2022-04-11 12:38:03 +0300

    Edited backend api

--- frontend/src/contexts/socket.js
+++ frontend/src/contexts/socket.js
@@ -2,8 +2,8 @@ import socketIOClient from "socket.io-client";
 
 import React from "react";
 
-const SOCKET_URL = 'http://54.183.235.87';
+const SOCKET_URL = 'https://api.cappedrange.gg';
 const LOCAL_SOCKET_URL = "http://localhost:5000"
 
-export const socket = socketIOClient(LOCAL_SOCKET_URL);
+export const socket = socketIOClient(SOCKET_URL);
 export const SocketContext = React.createContext();
